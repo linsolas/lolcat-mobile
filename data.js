e39af19dfc522a775555254e9ff2de79607d3936
Romain Linsolas <linsolas@gmail.com>
2013-10-20 23:26:54 +0200

    Update code

--- data.js
+++ data.js
@@ -18,18 +18,32 @@ var data_lolcats = [
 
 
     {
-        id: 'abc-123',
+        id: 'abc-01',
         name: 'Lolcat on the bike',
         status: STATUS.NO_VOTE,
         date: '2013-01-01',
         image: 'http://laughingsquid.com/wp-content/uploads/invisible-bike-20080517-191704.jpg'
     },
     {
-        id: 'abc-456',
+        id: 'abc-02',
         name: 'Lolcat on Facebook',
         status: STATUS.VALIDATED,
         date: '2013-01-01',
         image: 'http://fc03.deviantart.net/fs71/f/2010/179/6/b/Lol_Cat_Computer_by_strongbad_joe132.jpg'
+    },
+    {
+        id: 'abc-03',
+        name: 'Lolcat submission',
+        status: STATUS.VALIDATED,
+        date: '2013-01-01',
+        image: 'http://www.rubberrepublic.com/wp-content/uploads/2011/09/lolcat-funny-picture-moderator1.jpg'
+    },
+    {
+        id: 'abc-04',
+        name: 'Lolcat crazy',
+        status: STATUS.REFUSED,
+        date: '2013-01-01',
+        image: 'http://www.angelaslatter.com/wp-content/uploads/2010/08/crazy.jpg'
     }
 
 
